05f95687878b36df05fcec2715d2d4a68e25aee1
MahMoudMostaAfa <mahmoud2030m@gmail.com>
2025-11-29 16:48:42 +0200

    configure fluent validation , medtair , automapper , pipeline behaviours

--- src/Kawadar.Api/Controllers/V1/AuthController.cs
+++ src/Kawadar.Api/Controllers/V1/AuthController.cs
@@ -1,4 +1,6 @@
 
+using Kawadar.Application.Features.Auth.Commands.Register;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kawadar.Api.Controllers.V1;
@@ -8,10 +10,32 @@ namespace Kawadar.Api.Controllers.V1;
 
 public class AuthController : ApiController
 {
+
+  private readonly ISender _sender;
+  public AuthController(ISender sender)
+  {
+    _sender = sender;
+  }
   [HttpGet]
 
   public IActionResult Get()
   {
     return Ok("Auth Controller is working!");
   }
+
+  [HttpPost("register")]
+  public async Task<IActionResult> Register([FromBody] Request request)
+  {
+    var name = request.Name;
+    {
+      var RegisterCommand = new RegisterCommand(name);
+      var result = await _sender.Send(RegisterCommand);
+
+      return Ok(result);
+    }
+  }
+}
+public class Request
+{
+  public string Name { get; set; } = "";
 }
