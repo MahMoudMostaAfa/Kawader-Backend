7732d73d30fc6749a29681db738867e982a08f87
omartamer244@gmail.com <omartamer244@gmail.com>
2026-02-23 21:56:08 +0200

    Adding portfolio related entities unit tests

---
+++ tests/kawadar.Domain.UnitTests/Portfolio/PortfolioProjectViewTests.cs
@@ -0,0 +1,50 @@
+﻿
+using Azure.Core;
+using Kawadar.Domain.Portfolios.ProjectView;
+using Xunit;
+
+namespace kawadar.Domain.UnitTests.Portfolio
+{
+    public class PortfolioProjectViewTests
+    {
+        [Fact]
+        public void Create_ValidData_ShouldSucceed()
+        {
+            Guid userProfileId = Guid.NewGuid();
+            Guid projectId = Guid.NewGuid();
+
+            var result = PortfolioProjectView.Create(projectId, userProfileId);
+            Assert.True(result.IsSuccess);
+
+            var view = result.Value;
+            Assert.Equal(userProfileId, view.UserProfileId);
+            Assert.Equal(projectId, view.PortfolioProjectId);
+        }
+
+        [Fact]
+        public void Create_EmptyUserProfileId_ShouldFail()
+        {
+            Guid userProfileId = Guid.Empty;
+            Guid projectId = Guid.NewGuid();
+
+            var result = PortfolioProjectView.Create(projectId, userProfileId);
+            Assert.True(result.IsError);
+
+            Assert.Equal(PortfolioProjectViewErrors.ViewedByIdIsRequired.Code, result.TopError.Code);
+            Assert.Equal(PortfolioProjectViewErrors.ViewedByIdIsRequired.Description, result.TopError.Description);
+        }
+
+        [Fact]
+        public void Create_EmptyProjectId_ShouldFail()
+        {
+            Guid userProfileId = Guid.NewGuid();
+            Guid projectId = Guid.Empty;
+
+            var result = PortfolioProjectView.Create(projectId, userProfileId);
+            Assert.True(result.IsError);
+
+            Assert.Equal(PortfolioProjectViewErrors.PortfolioProjectIdIsRequired.Code, result.TopError.Code);
+            Assert.Equal(PortfolioProjectViewErrors.PortfolioProjectIdIsRequired.Description, result.TopError.Description);
+        }
+    }
+}
